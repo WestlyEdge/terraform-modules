d6939d6bcaf88edaa72812d4c27a724b4f0c5d50
WestlyEdge <westly.edge@gmail.com>
2017-07-24 23:06:06 -0500

    rename alb target group

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,7 +1,7 @@
-# Default ALB implementation that can be used connect ECS instances to it
+# Default ALB implementation that can be used to connect ECS instances to it
 
 resource "aws_alb_target_group" "default" {
-  name                 = "${var.alb_name}-default"
+  name                 = "${var.alb_name}-target-group"
   port                 = 80
   protocol             = "HTTP"
   vpc_id               = "${var.vpc_id}"
