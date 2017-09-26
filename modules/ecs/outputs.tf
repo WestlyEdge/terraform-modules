6e31897f8d5d4d049029289cf7ba2d672265d506
WestlyEdge <westly.edge@gmail.com>
2017-09-26 17:27:04 -0500

    removed call to alb module from ecs module, we will now create the alb per ecs service

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,17 +1,6 @@
-output "alb_target_group" {
-  value = "${module.alb.alb_target_group}"
-}
 
 output "ecs_cluster_arn" {
   value = "${aws_ecs_cluster.cluster.id}"
-}
-
-output "alb_name" {
-  value = "${module.alb.alb_name}"
-}
-
-output "alb_security_group_id" {
-  value = "${module.alb.alb_security_group_id}"
 }
 
 output "ecs_instance_security_group_id" {
