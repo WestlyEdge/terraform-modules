41d55570b4168b13a4b25a22090d566da46bc4c7
WestlyEdge <westly.edge@gmail.com>
2017-10-19 17:28:29 -0500

    testing override.tf file for debug-mode=true

--- modules/network/override.tf
+++ modules/network/override.tf
@@ -1,6 +1,7 @@
 
 # Temporarily open internet access in private vpc so we can ssh to instances
-resource "aws_route" "private_nat_route_debug_mode" {
+# You'll receive an error on terraform apply, so you'll need to taint this resource first
+resource "aws_route" "private_nat_route" {
   count                   = "${length(var.private_subnet_cidrs)}"
   route_table_id          = "${element(module.private_subnet.route_table_ids, count.index)}"
   gateway_id              = "${module.vpc.igw}"
