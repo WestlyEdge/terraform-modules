741519fb633628b9bc5c5d81233b82fc8556af4e
WestlyEdge <westly.edge@gmail.com>
2017-10-19 16:43:17 -0500

    testing override.tf file for debug-mode=true

--- modules/network/override.tf
+++ modules/network/override.tf
@@ -2,5 +2,4 @@
 # Temporarily open internet access in private vpc so we can ssh to instances
 resource "aws_route" "private_nat_route" {
   gateway_id        = "${module.vpc.igw}"
-  nat_gateway_id    = ""
 }
