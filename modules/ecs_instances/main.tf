8fa54fed66b2b90cb3aba9e9141e74c4f9e626ad
WestlyEdge <westly.edge@gmail.com>
2017-10-19 15:10:05 -0500

    testing override.tf file for debug-mode=true

--- modules/ecs_instances/main.tf
+++ modules/ecs_instances/main.tf
@@ -16,17 +16,6 @@ resource "aws_security_group" "instance" {
   }
 }
 
-# TODO : trigger this with an incoming flag maybe?
-# Temporarily add this rule so we can ssh to ecs hosts
-resource "aws_security_group_rule" "public-ssh-access" {
-  type                      = "ingress"
-  from_port                 = 22
-  to_port                   = 22
-  protocol                  = "TCP"
-  cidr_blocks               = ["0.0.0.0/0"]
-  security_group_id         = "${aws_security_group.instance.id}"
-}
-
 # We separate the rules from the aws_security_group because then we can manipulate the 
 # aws_security_group outside of this module
 resource "aws_security_group_rule" "outbound_internet_access" {
@@ -47,9 +36,6 @@ resource "aws_launch_configuration" "launch" {
   user_data                   = "${data.template_file.user_data.rendered}"
   iam_instance_profile        = "${var.iam_instance_profile_id}"
   key_name                    = "${var.key_name}"
-
-  # TODO : trigger this with an incoming flag maybe?
-  associate_public_ip_address = true  # Temporarily add public ip so we can ssh to ecs hosts
 
   # aws_launch_configuration can not be modified.
   # Therefore we use create_before_destroy so that a new modified aws_launch_configuration can be created before the old one gets destroyed
