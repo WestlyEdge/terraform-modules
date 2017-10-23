352492d87737d3b5dc3823e9755ccd77283099fd
WestlyEdge <westly.edge@gmail.com>
2017-10-23 18:46:48 -0500

    remove efs volume mount

--- modules/ecs_instances/templates/user_data.sh
+++ modules/ecs_instances/templates/user_data.sh
@@ -101,14 +101,6 @@ end script
 
 EOF
 
-# mount the efs volume to the ecs host instance
-yum install -y nfs-utils
-mkdir /efs
-echo "mounting efs volume - begin"
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-b5e8bcfc.efs.us-east-1.amazonaws.com:/ /efs
-echo "mounting efs volume - complete"
-service docker restart
-
 start ecs
 
 # set a unique ec2 tag name for this instance
